37dcbee625d057ed508d1ea960011ca54e49e15e
juanafrini12-dev <juanafrini12@gmail.com>
2026-05-17 17:44:42 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,15 @@
-
 export default function RootLayout({ children }) {
-  return (
+  return( 
     <html>
-      <body>
-        {children}
-
+      <head>
         <script
           type="text/javascript"
-          src="https://pl29470146.effectiveratecpm.com/82/28/xx.js"
+          src="https://pl29470146.effectiveratecpm.com/..."
         ></script>
+      </head>
+
+      <body>
+        {children}
       </body>
     </html>
   );
