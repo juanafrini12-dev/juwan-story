0fededd8a698f7ea536ce6b2d83b80c7dc134e13
juanafrini12-dev <juanafrini12@gmail.com>
2026-05-17 04:28:52 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,15 @@
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        {children}
+
+        <script
+          type="text/javascript"
+          src="https://pl29470146.effectiveratecpm.com/82/28/xx.js"
+        ></script>
+      </body>
+    </html>
+  );
+}
